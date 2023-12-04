5d7d924c1ed7bb192260c964f6713e4bf5641c70
Joaquin Eduardo Arreguez <joaquin.arreguez@naranjax.com>
2023-12-04 16:56:15 -0300

    feat(plsPlay): Fix model relationships and add ToDto method to Group

--- package/models/models.go
+++ package/models/models.go
@@ -1,6 +1,12 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"errors"
+	"strings"
+	"time"
+
+	"gorm.io/gorm"
+)
 
 type Group struct {
 	gorm.Model
@@ -11,14 +17,25 @@ type Group struct {
 	Transactions []Transaction `gorm:"foreignKey:Group"`
 }
 
+type GroupDTO struct {
+	CreatedAt    time.Time
+	UpdatedAt    time.Time
+	Name         string
+	Description  string
+	Users        string
+	Expenses     []Expense
+	Transactions []Transaction
+}
+
 type User struct {
 	gorm.Model
-	Name         string
-	Email        string
-	Groups       []*Group      `gorm:"many2many:user_groups;"`
-	Expenses     []Expense     `gorm:"foreignKey:Owner"`
-	Transactions []Transaction `gorm:"foreignKey:Sender"`
-	Participant  uint
+	Name                 string
+	Email                string
+	Groups               []*Group      `gorm:"many2many:user_groups;"`
+	Expenses             []Expense     `gorm:"foreignKey:Owner"`
+	ParticipatedExpenses []*Expense    `gorm:"many2many:expense_participants;"`
+	SenderTransactions   []Transaction `gorm:"foreignkey:SenderUserID"`
+	ReceiverTransactions []Transaction `gorm:"foreignkey:ReceiverUserID"`
 }
 
 type Expense struct {
@@ -27,14 +44,50 @@ type Expense struct {
 	Amount       float64
 	Group        uint
 	Owner        uint
-	Participants []User `gorm:"foreignKey:Participant"`
+	Participants []*User `gorm:"many2many:expense_participants;"`
 }
 
 type Transaction struct {
 	gorm.Model
-	Amount   float64
-	Group    uint
-	Sender   uint
-	Receiver uint
-	User     User `gorm:"foreignKey:Receiver"`
+	Amount         float64
+	Group          uint
+	SenderUserID   uint
+	ReceiverUserID uint
+	Sender         User `gorm:"foreignkey:SenderUserID"`
+	Receiver       User `gorm:"foreignkey:ReceiverUserID"`
+}
+
+func NewGroup(owner *User, name string, description string) (*Group, error) {
+	if owner == nil {
+		return nil, errors.New("Group owner (user) is required")
+	}
+	if name == "" {
+		return nil, errors.New("Group name is required")
+	}
+
+	return &Group{Name: name, Description: description, Users: []*User{owner}}, nil
+}
+
+func NewUser(name, email string) (*User, error) {
+	if name == "" {
+		return nil, errors.New("User name is required")
+	}
+	return &User{Name: name, Email: email}, nil
+}
+
+func (g *Group) ToDto() GroupDTO {
+	var usernames []string
+	for _, user := range g.Users {
+		usernames = append(usernames, user.Name)
+	}
+
+	return GroupDTO{
+		CreatedAt:    g.CreatedAt,
+		UpdatedAt:    g.UpdatedAt,
+		Name:         g.Name,
+		Description:  g.Description,
+		Expenses:     g.Expenses,
+		Transactions: g.Transactions,
+		Users:        strings.Join(usernames, ","),
+	}
 }
