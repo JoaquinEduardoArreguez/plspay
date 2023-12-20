02db20a9574d213701872637479ace524d1c9fd8
Joaquin Eduardo Arreguez <joaquin.arreguez@naranjax.com>
2023-12-20 14:43:09 -0300

    Refactor GroupHandler to use GroupService

--- src/api/group.handlers.go
+++ src/api/group.handlers.go
@@ -41,36 +41,13 @@ func (app *Application) createGroup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	name := form.Get("name")
-	participantsList := strings.Split(form.Get("participants"), ",")
+	participantsNames := strings.Split(form.Get("participants"), ",")
 	date, _ := time.Parse("2006-01-02", form.Get("date"))
-
-	var participants []models.User
-	dbErrorFindingUsers := app.userRepository.FindByNames(participantsList, &participants).Error
-	if errors.Is(dbErrorFindingUsers, gorm.ErrRecordNotFound) {
-		app.notFound(w)
-		return
-	} else if dbErrorFindingUsers != nil {
-		app.serverError(w, dbErrorFindingUsers)
-		return
-	}
-
 	groupOwner := app.authenticatedUser(r)
 
-	var participantsPtrs []*models.User
-	for _, participant := range participants {
-		participantsPtrs = append(participantsPtrs, &participant)
-	}
-
-	group, errorConstructingGroup := models.NewGroup(name, groupOwner, participantsPtrs, date)
-
-	if errorConstructingGroup != nil {
-		app.errorLog.Fatal(errorConstructingGroup)
-	}
-
-	insertGroupResponse := app.groupRepository.Create(group)
-
-	if insertGroupResponse.Error != nil {
-		app.serverError(w, insertGroupResponse.Error)
+	group, errorCreatingGroup := app.groupService.CreateGroup(name, groupOwner, participantsNames, date)
+	if errorCreatingGroup != nil {
+		app.errorLog.Fatal()
 	}
 
 	app.session.Put(r, "flash", "Group created!")
@@ -86,13 +63,11 @@ func (app *Application) showGroup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	group := &models.Group{}
-	dbError := app.groupRepository.GetByID(uint(id), group, "Users", "Expenses.Owner", "Expenses.Participants", "Transactions").Error
-
-	if errors.Is(dbError, gorm.ErrRecordNotFound) {
+	if err := app.groupRepository.GetByID(uint(id), group, "Users", "Expenses.Owner", "Expenses.Participants", "Transactions"); errors.Is(err, gorm.ErrRecordNotFound) {
 		app.notFound(w)
 		return
-	} else if dbError != nil {
-		app.serverError(w, dbError)
+	} else if err != nil {
+		app.serverError(w, err)
 		return
 	}
 
@@ -103,12 +78,11 @@ func (app *Application) showGroups(w http.ResponseWriter, r *http.Request) {
 	var userGroupsDtos []*models.GroupDTO
 
 	user := app.authenticatedUser(r)
-	dbResponse := app.userRepository.GetByID(user.ID, user, "Groups")
-	if errors.Is(dbResponse.Error, gorm.ErrRecordNotFound) {
+	if err := app.userRepository.GetByID(user.ID, user, "Groups"); errors.Is(err, gorm.ErrRecordNotFound) {
 		app.notFound(w)
 		return
-	} else if dbResponse.Error != nil {
-		app.serverError(w, dbResponse.Error)
+	} else if err != nil {
+		app.serverError(w, err)
 		return
 	}
 
