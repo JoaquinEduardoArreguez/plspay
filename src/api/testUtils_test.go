6ae52092dc5e32b0ce07c778ed91e701a6d94ac4
Joaquin Eduardo Arreguez <joaquin.arreguez@tarmac.io>
2024-01-12 17:27:06 -0300

    On testUtils: modify newTestApplication to add all new necessary dependencies, add postForm to test post requests, add initTestDatabase for tests that need a database

--- src/api/testUtils_test.go
+++ src/api/testUtils_test.go
@@ -7,8 +7,18 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"net/url"
 	"regexp"
 	"testing"
+	"time"
+
+	"github.com/JoaquinEduardoArreguez/plspay/package/models"
+	"github.com/JoaquinEduardoArreguez/plspay/package/services"
+	"github.com/golangcollege/sessions"
+	_ "github.com/jinzhu/gorm/dialects/postgres"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
 )
 
 var csrfTokenRX = regexp.MustCompile(`<input type='hidden' name='csrf_token' value="{{.CsrfToken}}" />`)
@@ -18,9 +28,28 @@ type testServer struct {
 }
 
 func newTestApplication(t *testing.T) *Application {
+	templateCache, err := newTemplateCache("./ui/html/")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	session := sessions.New([]byte("jaodh+pPbnzHbS*+9Pk8qGWhTzbpa@ps"))
+	session.Lifetime = 12 * time.Hour
+	session.Secure = true
+
+	database, initDatabaseError := initTestDatabase(t)
+	if initDatabaseError != nil {
+		t.Fatal(initDatabaseError)
+	}
+
 	return &Application{
-		errorLog: log.New(io.Discard, "", 0),
-		infoLog:  log.New(io.Discard, "", 0),
+		errorLog:       log.New(io.Discard, "", 0),
+		infoLog:        log.New(io.Discard, "", 0),
+		session:        session,
+		templateCache:  templateCache,
+		groupService:   services.NewGroupService(database),
+		expenseService: services.NewExpenseService(database),
+		userService:    services.NewUserService(database),
 	}
 }
 
@@ -47,13 +76,29 @@ func newTestServer(t *testing.T, h http.Handler) *testServer {
 func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byte) {
 	rs, err := ts.Client().Get(ts.URL + urlPath)
 	if err != nil {
-		t.Fatal(err)
+		t.Fatal("Error on request", err)
 	}
 	defer rs.Body.Close()
 	body, err := io.ReadAll(rs.Body)
 	if err != nil {
+		t.Fatal("Error reading body", err)
+	}
+	return rs.StatusCode, rs.Header, body
+}
+
+func (ts *testServer) postForm(t *testing.T, urlPath string, form url.Values) (int, http.Header, []byte) {
+	rs, err := ts.Client().PostForm(ts.URL+urlPath, form)
+	if err != nil {
 		t.Fatal(err)
 	}
+
+	defer rs.Body.Close()
+
+	body, err := io.ReadAll(rs.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 	return rs.StatusCode, rs.Header, body
 }
 
@@ -63,4 +108,29 @@ func extractCSRFToken(t *testing.T, body []byte) string {
 		t.Fatal("no csrf token found in body")
 	}
 	return html.UnescapeString(string(matches[1]))
+}
+
+func initTestDatabase(t *testing.T) (*gorm.DB, error) {
+	testDsn := "host=localhost user=postgresTest password=adminTest dbname=plspayTest port=5433 sslmode=disable"
+	database, openDatabaseError := gorm.Open(postgres.Open(testDsn), &gorm.Config{
+		TranslateError: true,
+	})
+	if openDatabaseError != nil {
+		return nil, openDatabaseError
+	}
+
+	database.Migrator().DropTable()
+
+	migrateDatabaseError := database.AutoMigrate(
+		&models.Group{},
+		&models.User{},
+		&models.Expense{},
+		&models.Transaction{},
+		&models.Balance{},
+	)
+	if migrateDatabaseError != nil {
+		return nil, migrateDatabaseError
+	}
+
+	return database, nil
 }
